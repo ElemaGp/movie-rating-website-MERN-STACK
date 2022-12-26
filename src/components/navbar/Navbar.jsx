3506a24db1cc2f4f0c12c09954ac6e0c6579bfdd
ElemaGp <elemagodspower@gmail.com>
2022-12-27 00:51:28 +0100

    feat: add search ui in the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import style from "./navbar.module.css"
 import React from 'react'
+import { SearchOutlined } from "@mui/icons-material"
 
 const Navbar = () => {
 
@@ -7,7 +8,10 @@ const Navbar = () => {
     <div className={style.navbarWrapper}>
       <h2 className={style.navbarLeft}>MRating</h2>
       <div className={style.navbarRight}>
-        <input type="text" />
+        <div className={style.search}>
+            <input type="text" style={{outline:"none", border:"none"}}/>
+            <SearchOutlined className={style.searchIcon}/>
+        </div>
             <img 
             src="https://img.freepik.com/free-photo/cheerful-indian-businessman-smiling-closeup-portrait-jobs-career-campaign_53876-129417.jpg?w=740&t=st=1672089771~exp=1672090371~hmac=c04641cada23f500b57f9b38fc066092e2cc9192e5e4dc463cdd1a7aec4ea888" 
             alt="" 
