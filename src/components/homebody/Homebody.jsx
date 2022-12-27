dd52136b5092c3d8acfa0f929b4aacc65218ee9d
ElemaGp <elemagodspower@gmail.com>
2022-12-27 19:57:11 +0100

    feat: added readme and dummy movie data

--- src/components/homebody/Homebody.jsx
+++ src/components/homebody/Homebody.jsx
@@ -1,10 +1,33 @@
 import style from "./homebody.module.css";
+import {movies} from "../../moviesArray"
 
 const Homebody = () => {
-    
+
   return (
-    <div>Homebody</div>
+    <div className={style.homebodyWrapper}>
+        <div className={style.homebodyMovies}>
+            <h2>MOVIES</h2>
+            <div className={style.moviesList}>
+                {
+                  movies.map((movie)=>(
+                    <div className={style.eachMovie} key={movie.id}>
+                      <img src={movie.image} alt="moviepic" />
+                      <h2>{movie.title}</h2>
+                      <p>{movie.desc}</p>
+                    </div>
+                  ))
+                }
+            </div>
+        </div>
+        <div className={style.homebodySeries}>
+            <h2>SERIES</h2>
+            <div className={style.seriesList}>
+
+            </div>
+        </div>
+    </div>
   )
 }
 
 export default Homebody
+            
