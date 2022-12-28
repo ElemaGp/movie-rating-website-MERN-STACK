c8242a46e5f68aed3b22f94cd9cf742974c34668
ElemaGp <elemagodspower@gmail.com>
2022-12-28 10:40:40 +0100

    feat: added series dummy data

--- src/components/homebody/Homebody.jsx
+++ src/components/homebody/Homebody.jsx
@@ -1,13 +1,14 @@
 import style from "./homebody.module.css";
 import {movies} from "../../moviesArray"
+import {series} from "../../moviesArray"
 
 const Homebody = () => {
 
   return (
     <div className={style.homebodyWrapper}>
         <div className={style.homebodyMovies}>
-            <h1>MOVIES</h1>
-            <div className={style.moviesList}>
+            <h1 className={style.homebodyText}>MOVIES</h1>
+            <div className={style.homebodyList}>
                 {
                   movies.map((movie)=>(
                     <div className={style.eachMovie} key={movie.id}>
@@ -20,9 +21,17 @@ const Homebody = () => {
             </div>
         </div>
         <div className={style.homebodySeries}>
-            <h2>SERIES</h2>
-            <div className={style.seriesList}>
-
+            <h1 className={style.homebodyText}>SERIES</h1>
+            <div className={style.homebodyList}>
+            {
+                  series.map((serie)=>(
+                    <div className={style.eachMovie} key={serie.id}>
+                      <img src={serie.image} alt="moviepic" className={style.movieImg}/>
+                      <h2>{serie.title}</h2>
+                      <p className={style.movieDesc}>{serie.desc}</p>
+                    </div>
+                  ))
+                }
             </div>
         </div>
     </div>
