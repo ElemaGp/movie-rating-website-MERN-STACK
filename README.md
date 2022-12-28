17b0fa42b73b9e6289aae90d20ee16f26d8f3e02
ElemaGp <elemagodspower@gmail.com>
2022-12-28 14:48:52 +0100

    style: styled the swiper

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## Material UI
 
 I used material ui for the sidebar drawer component (Sidebar component) for the sliding sidebar.
+
+## Swiperjs
+
+I used swiperjs for the slider on (Homebody) component.
+
+## Map
+
+I mapped through the movie data in the (Homebody) component
--- src/components/homebody/Homebody.jsx
+++ src/components/homebody/Homebody.jsx
@@ -2,25 +2,51 @@ import style from "./homebody.module.css";
 import {movies} from "../../moviesArray"
 import {series} from "../../moviesArray"
 
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+// import 'swiper/css/bundle';
 const Homebody = () => {
 
   return (
     <div className={style.homebodyWrapper}>
-        <div className={style.homebodyMovies}>
+        <div className={style.homebodyCover}>
             <h1 className={style.homebodyText}>MOVIES</h1>
-            <div className={style.homebodyList}>
+            <Swiper 
+            className={style.homebodyList}
+            // install Swiper modules
+            modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+            spaceBetween={50}
+            slidesPerView={3}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            autoplay = {{
+              delay: 2500,
+              disableOnInteraction: false,
+              }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
+            >
                 {
                   movies.map((movie)=>(
-                    <div className={style.eachMovie} key={movie.id}>
+                    <SwiperSlide className={style.eachMovie} key={movie.id}>
                       <img src={movie.image} alt="moviepic" className={style.movieImg}/>
                       <h2>{movie.title}</h2>
                       <p className={style.movieDesc}>{movie.desc}</p>
-                    </div>
+                    </SwiperSlide>
                   ))
                 }
-            </div>
+            </Swiper>
         </div>
-        <div className={style.homebodySeries}>
+        <div className={style.homebodyCover}>
             <h1 className={style.homebodyText}>SERIES</h1>
             <div className={style.homebodyList}>
             {
