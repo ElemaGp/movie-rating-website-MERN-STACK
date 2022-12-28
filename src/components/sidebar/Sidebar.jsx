8b6c4a99049c8d1ab42f4fdd403560625911f418
ElemaGp <elemagodspower@gmail.com>
2022-12-28 23:20:57 +0100

    feat: made the slider show 3 slides per page on large screens and 1 per page on small screens

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,12 +1,14 @@
 import style from "./sidebar.module.css"
 import React, { useState } from 'react'
-import { Box, Drawer, FormControl, FormControlLabel, IconButton, Link, Stack, Switch, Typography } from "@mui/material"
+import { Box, Button, Drawer, FormControl, FormControlLabel, IconButton, Link, Stack, Switch, Typography } from "@mui/material"
 import { MenuOutlined } from "@mui/icons-material";
+import useWindowSize from "../../useWindowSize";
 
 
 const Sidebar = () => {
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
     const [checked, setChecked] = useState(false);
+    const size = useWindowSize();
 
     console.log(checked);
 
@@ -29,6 +31,9 @@ const Sidebar = () => {
             <Link variant="h5" href="/" underline="hover" sx={{fontWeight:"700"}}>
                 SERIES
             </Link>
+
+            {size.width <= 600 && <Box mt={5} ><Button variant="contained" color="secondary"> LOGOUT </Button ></Box>}
+
             <Box mt={30}><FormControlLabel  label={<Typography variant="body1" color="primary" sx={{fontWeight:"500"}}>Dark mode</Typography>} control={<Switch checked={checked} onChange={handleChange}/>}/></Box>    {/*Getting the value from the "switch" element (note that whenever the switch is switched on, the "checked" prop value is true and whenever it is switched off, the "checked" prop value is false. I'm simply grabbing that value using my own "checked" useState using the handleChange function. ) */}
             
         </Stack>
