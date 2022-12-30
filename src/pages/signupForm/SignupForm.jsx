4ab7eb9debb6572353ffc256ed61795806ce3bbb
ElemaGp <elemagodspower@gmail.com>
2022-12-30 14:12:54 +0100

    feat: created signup form using formik

--- src/pages/signupForm/SignupForm.jsx
+++ src/pages/signupForm/SignupForm.jsx
@@ -8,15 +8,21 @@ import FormikControl from "../../components/formikComponents/FormikControl"
 
 function SignupForm () {
   const initialValues = {
+    firstName: '',
+    lastName: '',
     email: '',
-    password: ''
+    password: '',
+    confirmPassword: ''
   }
 
   const validationSchema = Yup.object({
+    firstName: Yup.string().required('First name is required'),
+    lastName: Yup.string().required("Last name is required"),
     email: Yup.string()
       .email('Invalid email format')
       .required('Required'),
-    password: Yup.string().required('Required')
+    password: Yup.string().required('Required'),
+    confirmPassword: Yup.string().oneOf([Yup.ref('password'), ''], 'Passwords much match').required('Required')
   })
 
   const onSubmit = values => {
@@ -35,17 +41,35 @@ function SignupForm () {
             <FormikControl
               control='input'
               // control='chakraInput'
+              type='text'
+              label='First Name'
+              name='firstName'    //the "name" has to match with the initial value's name above.
+            />
+            <FormikControl
+              control='input'
+              type='password'
+              label='Last Name'
+              name='lastName'    //the "name's" value has to match with the initial value's name above.
+            />
+            <FormikControl
+              control='input'
               type='email'
               label='Email'
-              name='email'
+              name='email'    //the "name's" value has to match with the initial value's name above.
             />
             <FormikControl
               control='input'
               type='password'
               label='Password'
-              name='password'
+              name='password'    //the "name's" value has to match with the initial value's name above.
             />
-            <button type='submit' disabled={!formik.isValid}>Submit</button>
+            <FormikControl
+              control='input'
+              type='password'
+              label='Confirm Password'
+              name='confirmPassword'    //the "name's" value has to match with the initial value's name above.
+            />
+            <button type='submit' disabled={!formik.isValid}>Submit</button> {/*"formik.isValid is false whenever there is an error" */}
           </Form>
         )
       }}
