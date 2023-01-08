34cac15bbd9c9fff90680d738ad0acdb058a44ca
ElemaGp <elemagodspower@gmail.com>
2023-01-08 17:07:19 +0100

    feat: created password eye-toggle feature for signup page

--- src/pages/signupForm/SignupForm.jsx
+++ src/pages/signupForm/SignupForm.jsx
@@ -5,9 +5,15 @@ import { Formik, Form } from 'formik'
 import * as Yup from 'yup'
 import FormikControl from "../../components/formikComponents/FormikControl"
 import { Button } from "@mui/material"
+import usePasswordToggle from "../../usePasswordToggle"
 
 
 function SignupForm () {
+
+  //password eye-toggle
+  const [passwordInputType, EyeIcon, confirmPasswordInputType, confirmPasswordEyeIcon] = usePasswordToggle(); //bringing in the values of "const inputType" and "const icon" which i returned from the usePasswordToggle custom hook as "const passwordInputType" and "const EyeIcon".
+  
+  
   const initialValues = {
     firstName: '',
     lastName: '',
@@ -68,22 +74,31 @@ function SignupForm () {
               error={Boolean(formik.errors.email) && Boolean(formik.touched.email)} //if both "formik.errors" and "formik.touched" are true, then the "error" prop is true.
               helperText={Boolean(formik.touched.email) && formik.errors.email} //if "formik.touched.email" is true, then display the "formik.errors" associated with the "email".
             />
+            <div className={style.passwordArea}>
             <FormikControl
               control='MuiInput'
-              type='password'
+              type={passwordInputType}
               label='Password'
               name='password'    //the "name's" value has to match with the initial value's name above.
               error={Boolean(formik.errors.password) && Boolean(formik.touched.password)} //if both "formik.errors" and "formik.touched" are true, then the "error" prop is true.
               helperText={Boolean(formik.touched.password) && formik.errors.password} //if "formik.touched.password" is true, then display the "formik.errors" associated with the "password".
             />
+            <div className={style.passwordEyeIcon}>{EyeIcon}</div>
+          </div>
+
+          <div className={style.passwordArea}>
             <FormikControl
               control='MuiInput'
-              type='password'
+              type={confirmPasswordInputType}
               label='Confirm Password'
               name='confirmPassword'    //the "name's" value has to match with the initial value's name above.
               error={Boolean(formik.errors.confirmPassword) && Boolean(formik.touched.confirmPassword)} //if both "formik.errors" and "formik.touched" are true, then the "error" prop is true.
               helperText={Boolean(formik.touched.confirmPassword) && formik.errors.confirmPassword} //if "formik.touched.confirmPassword" is true, then display the "formik.errors" associated with the "confirmPassword".
             />
+            <div className={style.passwordEyeIcon}>{confirmPasswordEyeIcon}</div>
+          </div>
+
+          
             {/* <button type='submit' disabled={!formik.isValid} className={style.btn}>Sign Up</button> "formik.isValid is false whenever there is an error" */}
             <Button type="submit" variant="contained" color="primary" size="large" disabled={!formik.dirty || !formik.isValid}>Sign Up</Button>
           </Form>
